dbc9e0ce8f106bc469036a42b0077e195df77ffc
Ahmad Miftachul Hidayat <miftachul.mkb@gmail.com>
2025-03-12 05:54:19 +0700

    change import component to use await

--- src/app/view/[name]/page.tsx
+++ src/app/view/[name]/page.tsx
@@ -1,32 +1,27 @@
-import dynamic from "next/dynamic";
-import path from "path";
-
 export async function generateStaticParams() {
   return [
-    { id: "sidebar-demo" },
-    { id: "sidebar-header" },
-    { id: "sidebar-footer" },
-    { id: "sidebar-group " },
-    { id: "sidebar-group-collapsible" },
-    { id: "sidebar-group-action" },
-    { id: "sidebar-menu" },
-    { id: "sidebar-menu-action" },
-    { id: "sidebar-menu-sub" },
-    { id: "sidebar-menu-collapsible" },
-    { id: "sidebar-menu-badge" },
-    { id: "sidebar-controlled" },
-    { id: "sidebar-07" },
+    { name: "sidebar-demo" },
+    { name: "sidebar-header" },
+    { name: "sidebar-footer" },
+    { name: "sidebar-group" },
+    { name: "sidebar-group-collapsible" },
+    { name: "sidebar-group-action" },
+    { name: "sidebar-menu" },
+    { name: "sidebar-menu-action" },
+    { name: "sidebar-menu-sub" },
+    { name: "sidebar-menu-collapsible" },
+    { name: "sidebar-menu-badge" },
+    { name: "sidebar-controlled" },
+    { name: "sidebar-07" },
   ];
 }
 
 export const dynamicParams = false;
 
-const dir = "../../../blocks/";
-
 export default async function View({ params }: { params: Promise<{ name: string }> }) {
   const name = (await params).name;
-  const file = path.join(dir, `${name}.tsx`);
-  const Component = dynamic(() => import(file));
+  const mod = await import(`@/blocks/${name}.tsx`);
+  const Component = mod.default;
 
   return <Component />;
 }
--- src/components/component-preview.tsx
+++ src/components/component-preview.tsx
@@ -1,4 +1,3 @@
-import dynamic from "next/dynamic";
 import fs from "fs";
 import path from "path";
 import { styled } from "styled-system/jsx";
@@ -13,7 +12,8 @@ const dir = path.join(process.cwd(), "src/components/examples");
 export default async function ComponentPreview({ name }: ComponentPreviewProps) {
   const file = path.join(dir, `${name}.tsx`);
   const content = fs.readFileSync(file, "utf8");
-  const Component = dynamic(() => import(`./examples/${name}`));
+  const mod = await import(`@/components/examples/${name}.tsx`);
+  const Component = mod.default;
 
   return (
     <styled.div css={{ rounded: "md", borderWidth: "1px", my: "4", overflow: "hidden" }}>
