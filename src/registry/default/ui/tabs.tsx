3c97bf7d3ce24382dc6b8d28caf665eec250914a
Ahmad Miftachul Hidayat <miftachul.mkb@gmail.com>
2025-10-28 16:18:09 +0700

    refactor(tabs): use styled-system context

--- src/registry/default/ui/tabs.tsx
+++ src/registry/default/ui/tabs.tsx
@@ -1,35 +1,33 @@
 "use client";
 
-import * as React from "react";
 import * as TabsPrimitive from "@radix-ui/react-tabs";
-import { cx } from "styled-system/css";
-import { styled } from "styled-system/jsx";
+import { createStyleContext } from "styled-system/jsx";
 import { tabs } from "styled-system/recipes";
 
-const classes = tabs();
+const { withProvider, withContext } = createStyleContext(tabs);
 
-function Root({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.Root>) {
-  return <TabsPrimitive.Root className={cx(classes.root, className)} {...props} />;
-}
-const Tabs = styled(Root);
-Tabs.displayName = TabsPrimitive.Root.displayName;
+const Tabs = withProvider(TabsPrimitive.Root, "root", {
+  defaultProps: {
+    "data-slot": "tabs",
+  } as Partial<React.ComponentProps<typeof TabsPrimitive.Root>>,
+});
 
-function List({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.List>) {
-  return <TabsPrimitive.List className={cx(classes.list, className)} {...props} />;
-}
-const TabsList = styled(List);
-TabsList.displayName = TabsPrimitive.List.displayName;
+const TabsList = withContext(TabsPrimitive.List, "list", {
+  defaultProps: {
+    "data-slot": "tabs-list",
+  } as Partial<React.ComponentProps<typeof TabsPrimitive.List>>,
+});
 
-function Trigger({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.Trigger>) {
-  return <TabsPrimitive.Trigger className={cx(classes.trigger, className)} {...props} />;
-}
-const TabsTrigger = styled(Trigger);
-TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
+const TabsTrigger = withContext(TabsPrimitive.Trigger, "trigger", {
+  defaultProps: {
+    "data-slot": "tabs-trigger",
+  } as Partial<React.ComponentProps<typeof TabsPrimitive.Trigger>>,
+});
 
-function Content({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.Content>) {
-  return <TabsPrimitive.Content className={cx(classes.content, className)} {...props} />;
-}
-const TabsContent = styled(Content);
-TabsContent.displayName = TabsPrimitive.Content.displayName;
+const TabsContent = withContext(TabsPrimitive.Content, "content", {
+  defaultProps: {
+    "data-slot": "tabs-content",
+  } as Partial<React.ComponentProps<typeof TabsPrimitive.Content>>,
+});
 
 export { Tabs, TabsList, TabsTrigger, TabsContent };
